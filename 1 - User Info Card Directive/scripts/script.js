f4e15c46c5870ab7fad6c808cad3890acfda8960
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-09-07 06:56:06 +0100

    (1) Added bindings and using built-in directives with user info card custom directive

---
+++ 1 - User Info Card Directive/scripts/script.js
@@ -0,0 +1,24 @@
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function($scope) {
+    $scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rebel Base',
+            planet: 'Yavin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+});
+
+angular.module('app').directive('userInfoCard', function() {
+    return {
+        template: "Name: {{ user.name }} <br/><br /> <div ng-show='!!user.address'>Address: <br />{{ user.address.street}} <br /> {{ user.address.city }} <br /> {{ user.address.planet }}</div> <br /> <div>Friends: <div ng-repeat='friend in user.friends'>{{friend}}</div></div>",
+        restrict: "E"
+    } 
+});
