2e104b5e7874f1778fe0c7043839682d45feb667
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-09-08 19:43:18 +0100

    (2) Module 2 - Events, Scope and Controllers complete

--- 1 - User Info Card Directive/scripts/script.js
+++ 1 - User Info Card Directive/scripts/script.js
@@ -1,7 +1,7 @@
 angular.module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope) {
-    $scope.user = {
+    $scope.user1 = {
         name: 'Luke Skywalker',
         address: {
             street: 'PO Box 123',
@@ -13,12 +13,89 @@ angular.module('app').controller('mainCtrl', function($scope) {
             'Leia',
             'Chewbacca'
         ]
-    };
+    };  
+    
+    $scope.user2 = {
+        name: 'Han Solo',
+        address: {
+            street: 'PO Box 123',
+            city: 'Mo Eisley',
+            planet: 'Tattoine'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };   
 });
 
 angular.module('app').directive('userInfoCard', function() {
     return {
         templateUrl: "userInfoCard.html",
-        restrict: "E"
+        restrict: "E",
+        scope: {
+            user: '=', // uses the user attribute contents to dictate what goes into $scope.user on the isolated scope
+            initialCollapsed: '@collapsed' // uses the value passed in from the collapsed attribute. Data can only be passed in as a string            
+        }, // isolated scope
+        controller: function($scope) {
+            $scope.collapsed = ($scope.initialCollapsed === 'true');
+            
+            $scope.knightMe = function(user) {
+                user.rank = "knight";
+            };
+            
+            $scope.collapse = function() {
+                $scope.collapsed = !$scope.collapsed;
+            };    
+            
+            $scope.removeFriend = function(friend) {
+                var idx = $scope.user.friends.indexOf(friend);
+                if (idx > -1) {
+                    $scope.user.friends.splice(idx, 1);
+                }
+            };
+        }
+    } 
+});
+
+angular.module('app').directive('removeFriend', function() {
+    return {
+        restrict: "E",
+        templateUrl: "removeFriend.html",
+        scope: {
+            notifyParent: '&method' // uses the function name passed in via the method attribute
+        },
+        controller: function($scope) {
+            $scope.removing = false;
+            $scope.startRemove = function() {
+                $scope.removing = true;
+            };
+            $scope.cancelRemove = function() {
+                $scope.removing  = false;
+            };    
+            $scope.confirmRemove = function() {
+                $scope.notifyParent();
+            };
+        }
+    }
+});
+
+angular.module('app').directive('address', function() {
+    return {
+        restrict: "E",
+        templateUrl: 'address.html',
+        scope: true, // inherited scope, by default this is shared scope
+        controller: function($scope) {
+            $scope.collapsed = true;
+            
+            $scope.collapseAddress = function() {
+                $scope.collapsed = true;
+            };
+            
+            $scope.expandAddress = function() {
+                $scope.collapsed = false;  
+            };
+        }
     } 
 });
