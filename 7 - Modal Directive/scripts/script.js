a0c963da5ff528e62e6671a55f3f74f2cc32b4f9
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-09-17 20:09:56 +0100

    (7) Module 7 - Directives and Services complete

---
+++ 7 - Modal Directive/scripts/script.js
@@ -0,0 +1,65 @@
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function($scope) {
+    $scope.openModal = function() {
+        $scope.modalOpen = true;        
+    };
+    
+    $scope.closeModal = function(response) {
+        $scope.modalOpen = false;
+        console.log('modal closed', response);
+    };
+    
+    $scope.modalClosed = function(response) {
+        $scope.closeModal('no');
+    };
+});
+
+angular.module('app').controller('modalCtrl', function($scope) {
+    $scope.close = function(response) {
+        $scope.closeModal(response);
+    };
+});
+
+angular.module('app').directive('modal', function($document) {
+    return {
+        scope: {
+            modalOpen: '=open',
+            options: '=',
+            onClose: '&'
+        },
+        transclude: true,
+        templateUrl: 'modal.html',
+        controller: function($scope) {
+            $scope.close = function() {
+                $scope.modalOpen = false;
+                $scope.onClose();
+            };
+        },
+        link: function($scope, el, attrs) {
+            var options = angular.extend({
+                height: '250',
+                width: '500',
+                top: '20%',
+                left: '30%'
+            }, $scope.options);
+            
+            el.find('.modal-container').css({
+                'left': options.left,
+                'top': options.top,
+                'height': options.height + 'px',
+                'width': options.width + 'px'
+            });
+            
+            var pageHeight = $document.height();
+            var pageWidth = $document.width();
+            
+            el.find('.modal-blackout').css({
+                'width': pageWidth + 'px',
+                'height': pageHeight + 'px'
+            });
+            
+            
+        }
+    } 
+});
